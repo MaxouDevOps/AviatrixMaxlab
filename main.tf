93c04abd4167a5ff20d37139f8012b4f8dd21967
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-10-31 11:12:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,27 +18,50 @@ module "transit_aws_1" {
   
 }
 
-# Build the AWS Spokes
+#  Build the AWS Spokes
 
-module "spoke_aws" {
-  source  = "Eskimoodigital/aws-spoke-ec2/aviatrix"
-  version = "1.0.14"
+# module "spoke_aws" {
+#   source  = "Eskimoodigital/aws-spoke-ec2/aviatrix"
+#   version = "1.0.14"
 
-  count = 2
+#   count = 2
 
-  name            = "avffsp${count.index}"
-  cidr            = var.spoke_cidrs[count.index]
-  region          = "eu-central-1"
-  account         = "Eskimoo"
-  transit_gw      = "avx-avffhub-transit"
-  vpc_subnet_size = "24"
+#   name            = "avffsp${count.index}"
+#   cidr            = var.spoke_cidrs[count.index]
+#   region          = "eu-central-1"
+#   account         = "Eskimoo"
+#   transit_gw      = "avx-avffhub-transit"
+#   vpc_subnet_size = "24"
+
+#   ha_gw = "false"
+#   instance_size = "t2.micro"
+
+#   ec2_key = "KP_AVI_EC2_SPOKE"
+# }
+
+
+module "transit_azure_1" {
+  source  = "terraform-aviatrix-modules/azure-transit/aviatrix"
+  version = "4.0.1"
+
+  cidr = "10.78.0.0/20"
+  region = "West Europe"
+  account = "EskimooAzure"
 
   ha_gw = "false"
-  instance_size = "t2.micro"
+  name = "avazhub"
 
-  ec2_key = "KP_AVI_EC2_SPOKE"
 }
+  
+module "transit-peering" {
+  source  = "terraform-aviatrix-modules/mc-transit-peering/aviatrix"
+  version = "1.0.4"
 
+  transit_gateways = [
+    "avx-avazhub-transit",
+    "avx-avffhub-transit",
+    
+  ]
 
   
-  
+}
